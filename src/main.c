742dced9a6267c081e3630575d9b4741d58aa360
János Euler <42710663+Mac10goesBRRRT@users.noreply.github.com>
2023-12-01 16:25:20 +0100

    🐳🐳🐳🐳

--- src/main.c
+++ src/main.c
@@ -2,45 +2,174 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
-#define LED1 5 // port pin of on - board LED
-#define LED2 4
-#define LED3 26
+#include "driver/i2c.h"
 
-void blink(void * param);
+#define I2C_MASTER_PORT 0         // port number
+#define I2C_MASTER_SDA_IO 21      // GPIO data
+#define I2C_MASTER_SCL_IO 22      // GPIO clock
+#define I2C_MASTER_FREQ_HZ 400000 // I2C frequency
+
+#define ACK 1
+#define NACK 0
+
+i2c_config_t conf = {
+    .mode = I2C_MODE_MASTER,
+    .sda_io_num = I2C_MASTER_SDA_IO,
+    .sda_pullup_en = GPIO_PULLUP_ENABLE,
+    .scl_io_num = I2C_MASTER_SCL_IO,
+    .scl_pullup_en = GPIO_PULLUP_ENABLE,
+    .master.clk_speed = I2C_MASTER_FREQ_HZ};
+
+uint8_t i2c_register_read(
+    i2c_port_t port_num,
+    uint8_t dev_addr,
+    uint8_t reg_addr);
+
+static esp_err_t i2c_register_write(
+    i2c_port_t port_num,
+    uint8_t dev_addr,
+    uint8_t reg_addr,
+    uint8_t data);
+
+static esp_err_t i2c_multi_register_read(
+    i2c_port_t port_num,
+    uint8_t dev_addr,
+    uint8_t reg_addr,
+    uint8_t *data,
+    size_t size);
+
+uint8_t lux_read_sensor_id();
+ u_int16_t lux_read_sensor_value(u_int8_t reg, u_int8_t addr);
+
+void read_write_i2c (void * pvParameters);
 
 void app_main()
 {
-    gpio_set_direction(LED1, GPIO_MODE_OUTPUT); // set GPIO as output
-    gpio_set_direction(LED2, GPIO_MODE_OUTPUT); // set GPIO as output
-    gpio_set_direction(LED3, GPIO_MODE_OUTPUT); // set GPIO as output
-
-    static uint16_t leuchte1[] = {4, 1000};
-    TaskHandle_t xHandle1 = NULL;
-    xTaskCreate(&blink, "blink1", 4000, &leuchte1, tskIDLE_PRIORITY, &xHandle1);
-    configASSERT(xHandle1);
-
-    static uint16_t leuchte2[] = {5, 100};
-    TaskHandle_t xHandle2 = NULL;
-    xTaskCreate(&blink, "blink2", 4000, &leuchte2, tskIDLE_PRIORITY, &xHandle2);
-    configASSERT(xHandle2);
-
-    static uint16_t leuchte3[] = {26, 10};
-    TaskHandle_t xHandle3 = NULL;
-    xTaskCreate(&blink, "blink3", 4000, &leuchte3, tskIDLE_PRIORITY, &xHandle3);
-    configASSERT(xHandle3);
+    i2c_param_config(I2C_MASTER_PORT, &conf);
+    i2c_driver_install(I2C_MASTER_PORT, conf.mode, 0, 0, 0);
+    //printf("0x%x\n",i2c_register_read(I2C_MASTER_PORT, 0x29, 0x0));
+    i2c_register_write(I2C_MASTER_PORT, 0x29, 0x0, 0x3);
+    i2c_register_write(I2C_MASTER_PORT, 0x29, 0x1, 0b00010010);
+    printf("beep boop hell hier: %d\n", lux_read_sensor_value(0x29,0));
+    /*TaskHandle_t xtask_main = NULL;
+    xTaskCreate(&read_write_i2c, "readWritei2c", 4000, NULL, tskIDLE_PRIORITY, &xtask_main);
+    configASSERT(xtask_main);*/
 }
 
-
-void blink(void *param)
-{
-    bool state = 1;
-    uint16_t* localarray = (uint16_t*)param;
-    for (;;)
-    {
-        gpio_set_level(localarray[0], state);
-        vTaskDelay(localarray[1] / portTICK_PERIOD_MS);
-        state = !state;
-        //gpio_set_level(localarray[0], 0);
-        //vTaskDelay(localarray[1] / portTICK_PERIOD_MS);
+void read_write_i2c (void * pvParameters){
+    for(;;){
+        // printf("0x%x\n",lux_read_sensor_id());
+        //printf("0x%x\n", i2c_register_read(I2C_MASTER_PORT, 0x29, 0xA));
+        //printf("0x%x\n", i2c_register_read(I2C_MASTER_PORT, 0x29, 0xC));
+        //i2c_register_write(I2C_MASTER_PORT,0x29,0x2,0x0);
+        //printf("0x%x\n", i2c_register_read(I2C_MASTER_PORT, 0x29, 0xD));
+        //uint8_t data[2];
+        //i2c_multi_register_read(I2C_MASTER_PORT,0x29,0xC,&data,2);
+        //printf("0x%d%d\n", data[0],data[1]);
+        printf("beep boop hell hier: %d\n", lux_read_sensor_value(0x29,0));
+        //lux_read_sensor_value(1, 0x29);
+        vTaskDelay(200); //2 Sekunden
     }
 }
+
+
+uint8_t i2c_register_read(
+    i2c_port_t port_num,
+    uint8_t dev_addr,
+    uint8_t reg_addr)
+{
+    uint8_t data;
+    // port_num I2C Port Num, dev_addr Slave I2C, reg_addr register I2C
+    reg_addr += 128;
+    i2c_cmd_handle_t cmd;
+    uint8_t addr = dev_addr << 1;
+
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr, ACK);
+    i2c_master_write_byte(cmd, reg_addr, ACK);
+    i2c_master_start(cmd);
+    addr += 1;
+    i2c_master_write_byte(cmd, addr, ACK);
+    i2c_master_read_byte(cmd, &data, NACK);
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_MASTER_PORT, cmd, 10);
+    i2c_cmd_link_delete(cmd);
+
+    return data;
+}
+
+uint8_t lux_read_sensor_id()
+{
+    for (uint8_t i = 0; i < 0xFF; i++)
+    {
+        uint8_t data = i2c_register_read(I2C_MASTER_PORT, i, 0xA);
+        if (data == 0x50)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+static esp_err_t i2c_register_write(
+    i2c_port_t port_num,
+    uint8_t dev_addr,
+    uint8_t reg_addr,
+    uint8_t data)
+{
+    //port_num i2c port, dev_addr Slave addr
+    reg_addr += 128;
+    i2c_cmd_handle_t cmd;
+    uint8_t addr = dev_addr << 1;
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr, ACK);
+    i2c_master_write_byte(cmd, reg_addr, ACK);
+    i2c_master_write_byte(cmd, data, ACK);
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_MASTER_PORT, cmd, 10);
+    i2c_cmd_link_delete(cmd);
+
+    return 0;
+}
+
+static esp_err_t i2c_multi_register_read(
+    i2c_port_t port_num,
+    uint8_t dev_addr,
+    uint8_t reg_addr,
+    uint8_t *data,
+    size_t size){
+    // port_num I2C Port Num, dev_addr Slave I2C, reg_addr register I2C
+    reg_addr += 128;
+    i2c_cmd_handle_t cmd;
+    uint8_t addr = dev_addr << 1;
+    cmd = i2c_cmd_link_create();
+    //Start + Slave Addr + Write
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr, ACK);
+    //Register Addr
+    i2c_master_write_byte(cmd, reg_addr, ACK);
+    //Start + Slave Addr + Read
+    addr += 1;
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, addr, ACK);
+    //Jetzt lesen
+    i2c_master_read(cmd, data, size, NACK);
+    i2c_master_stop(cmd);
+    i2c_master_cmd_begin(I2C_MASTER_PORT, cmd, 10);
+    i2c_cmd_link_delete(cmd);
+
+    return 0;
+    }
+
+    u_int16_t lux_read_sensor_value(u_int8_t addr, u_int8_t reg){
+        reg = reg*2 + 0xC;
+        uint8_t data[2];
+        i2c_multi_register_read(I2C_MASTER_PORT, addr, reg, &data, 2);
+        u_int16_t value = data[1];
+        value = value << 8;
+        value = value | data[0];
+        return value;
+    }
+    
